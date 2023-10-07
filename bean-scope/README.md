f5bdac12fbccd63b7ba1e76a057d494fc2ab731e
zhangquan <zhangquan@hupu.com>
2023-10-07 10:20:21 +0800

    feat: Spring Bean 使用域：request 作用域

---
+++ bean-scope/README.md
@@ -0,0 +1,11 @@
+### Spring Bean 作用域之："request" Bean作用域
+
+```bash
+cd /Users/zhangquan/code/github.com/spring-framework-study
+
+mvn clean package 
+
+java -jar bean-scope/target/bean-scope-1.0-SNAPSHOT-war-exec.jar
+
+java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9527 bean-scope/target/bean-scope-1.0-SNAPSHOT-war-exec.jar
+```
---
+++ bean-scope/src/main/java/com/zq/bean/scope/web/WebConfiguration.java
@@ -0,0 +1,31 @@
+package com.zq.bean.scope.web;
+
+import com.zq.spring.ioc.overview.domain.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Web MVC 配置类
+ *
+ * @author <quanzhang875@gmail.com>
+ * @since  2023-10-01 22:08:39
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfiguration {
+
+	@Bean
+	 @RequestScope
+	//@SessionScope
+	//@ApplicationScope
+	public User user() {
+		User user = new User();
+		user.setId(1L);
+		user.setName("张权");
+		return user;
+	}
+
+}
