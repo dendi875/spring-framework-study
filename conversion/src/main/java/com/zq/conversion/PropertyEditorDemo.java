a8b965464a8361c45d323ddc324a669fe012dcbb
zhangquan <zhangquan@hupu.com>
2023-10-15 22:50:34 +0800

    feat: Spring 类型转换：基于Java Beans 接口的类型转换

---
+++ conversion/src/main/java/com/zq/conversion/PropertyEditorDemo.java
@@ -0,0 +1,32 @@
+package com.zq.conversion;
+
+import java.beans.PropertyEditor;
+
+/**
+ * 视频：161丨基于JavaBeans接口的类型转换：Spring是如何扩展PropertyEditor接口实现类型转换的？.mp4
+ * PPT: 第十五章 Spring 类型转换（Type Conversion）.pdf
+ *
+ * 基于 JavaBeans 接口的类型转换
+ * • 核心职责
+ * 		• 将 String 类型的内容转化为目标类型的对象
+ * • 扩展原理
+ * 		• Spring 框架将文本内容传递到 PropertyEditor 实现的 setAsText(String) 方法
+ * 		• PropertyEditor#setAsText(String) 方法实现将 String 类型转化为目标类型的对象
+ * 		• 将目标类型的对象传入 PropertyEditor#setValue(Object) 方法
+ * 		• PropertyEditor#setValue(Object) 方法实现需要临时存储传入对象
+ * 		• Spring 框架将通过 PropertyEditor#getValue() 获取类型转换后的对象
+ *
+ * @author <a href="mailto:quanzhang875@gmail.com">quanzhang875</a>
+ * @since  2023-10-15 22:41:00
+ */
+public class PropertyEditorDemo {
+	public static void main(String[] args) {
+		// 模拟 Spring Framework 操作
+		// 假设有一段文本
+		String name = "name = 张权";
+		PropertyEditor propertyEditor =  new StringToPropertiesPropertyEditor();
+		propertyEditor.setAsText(name);
+
+		System.out.println(propertyEditor.getValue());
+	}
+}
---
+++ conversion/src/main/java/com/zq/conversion/StringToPropertiesPropertyEditor.java
@@ -0,0 +1,31 @@
+package com.zq.conversion;
+
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorSupport;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Properties;
+
+/**
+ * 模拟 String to Properties 的 {@link PropertyEditor} 实现
+ *
+ * 1. 实现 setAsText(String) 方法
+ * 2. 将 String 类型转换成 Properties 类型
+ * 3. 临时存储 Properties 对象
+ * @author <a href="mailto:quanzhang875@gmail.com">quanzhang875</a>
+ * @since  2023-10-15 22:42:45
+ */
+public class StringToPropertiesPropertyEditor extends PropertyEditorSupport implements 	PropertyEditor {
+
+	@Override
+	public void setAsText(String text) throws java.lang.IllegalArgumentException {
+		Properties properties = new Properties();
+		try {
+			properties.load(new StringReader(text));
+		} catch (IOException e) {
+			throw new IllegalArgumentException();
+		}
+
+		setValue(properties);
+	}
+}
