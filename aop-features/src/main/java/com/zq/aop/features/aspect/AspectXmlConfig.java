26ddcb6323ae4d6c7dccfebd9418dfb4633f3326
zhangquan <zhangquan@hupu.com>
2023-10-23 18:51:33 +0800

    feat: 基于XML方式使用Around

--- aop-features/src/main/java/com/zq/aop/features/aspect/AspectXmlConfig.java
+++ aop-features/src/main/java/com/zq/aop/features/aspect/AspectXmlConfig.java
@@ -1,5 +1,7 @@
 package com.zq.aop.features.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 /**
  * 基于 XML 配置 Advice
  *
@@ -8,7 +10,13 @@ package com.zq.aop.features.aspect;
  */
 public class AspectXmlConfig {
 
+	public Object aroundAnyPublicMethod(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("@Around any public method: " + pjp.getSignature());
+		return pjp.proceed();
+	}
+
 	public void beforeAnyPublicMethod() {
 		System.out.println("@Before any public method.");
 	}
+
 }
